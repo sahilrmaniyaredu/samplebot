7625cbfb9e2bbefb0cc441d4b0e8aa96afd75579
sahilrmaniyaredu <76579294+sahilrmaniyaredu@users.noreply.github.com>
2021-05-05 22:12:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+import chatterbot
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer    
+chatbot = ChatBot(
+    'Nitr Chatbot',  
+     )
+
+from chatterbot.trainers import ListTrainer
+
+while True:
+    try:
+        user_input = input("you:")
+        chatbot_response=chatbot.get_response(user_input)
+        print("NITRbot:",chatbot_response)
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
 from chatbot import chatbot
 from flask import Flask, render_template, request
 
